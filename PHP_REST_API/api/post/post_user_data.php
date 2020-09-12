30b8da2f0ecf59d1f8e60a2ea567f3279e697bce
deivitrix <67457502+deivitrix@users.noreply.github.com>
2020-09-12 16:56:55 -0500

    Update post_user_data.php

--- PHP_REST_API/api/post/post_user_data.php
+++ PHP_REST_API/api/post/post_user_data.php
@@ -15,9 +15,9 @@ $db = $database->connect();
 //INSTANTIATE BLOG POST OBJECT
 $post = new doctor($db);
 
-if(isset($_GET['user_email'])&&isset($_GET['user_'])){
+if(isset($_GET['user_email'])&&isset($_GET['user_dni'])&&isset($_GET['user_names'])&&isset($_GET['user_last_names'])&&isset($_GET['birthdate'])&&isset($_GET['cellphone'])&&isset($_GET['sex'])&&isset($_GET['user_type'])){
 
-//$post -> post_user_data($_GET['user_data'],$_GET['business_ruc'],$_GET['role']);
+$post -> post_user_data($_GET['user_email'],$_GET['user_dni'],$_GET['user_names'],$_GET['user_last_names'],$_GET['birthdate'],$_GET['cellphone'],$_GET['sex'],$_GET['user_type']);
 
 }else{
     echo json_encode(
