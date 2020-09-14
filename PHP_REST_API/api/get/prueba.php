bf0d9748967891fa25b68edef5351cc8bd2f1603
GrougalQatal <71042478+GrougalQatal@users.noreply.github.com>
2020-09-14 12:33:19 -0500

    Update prueba.php

--- PHP_REST_API/api/get/prueba.php
+++ PHP_REST_API/api/get/prueba.php
@@ -28,28 +28,5 @@ header('Content-Type: application/json');  //FOR USE JSON
 
 
 
-    // //POST ARRAY
-    // $post_arraylist = array();
-    // $FinalArray = array();
-    // $jsontype['JsonType'] = array();
-    // $post_arraylist['DATOS_CLIENTE'] = array();
-
-
-    //     $post_item = array(
-    //         'cedula_ruc' => '',
-    //         'nombres' => '',
-    //         'apellido_pat' =>'',
-    //         'apellido_mat' => '',
-    //         'fecha_registro' => ''
-    //     );
-    //     //PUSH TO DATA
-    //     array_push($post_arraylist['DATOS_CLIENTE'], $post_item);
-    //     array_push($FinalArray,$jsontype);
-    //     array_push($FinalArray,$post_arraylist['DATOS_CLIENTE']);
-        
-    // //TURN IT TO JSON & OUTPUT
-    // echo json_encode($FinalArray);
-
-
 
 ?>
