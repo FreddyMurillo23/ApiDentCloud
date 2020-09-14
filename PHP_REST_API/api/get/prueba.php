dfb72fe9b5834e8512003cd476df3e91a47d4d02
GrougalQatal <71042478+GrougalQatal@users.noreply.github.com>
2020-09-14 12:19:45 -0500

    Update prueba.php

--- PHP_REST_API/api/get/prueba.php
+++ PHP_REST_API/api/get/prueba.php
@@ -6,9 +6,8 @@ header('Content-Type: application/json');  //FOR USE JSON
 
 
     //POST ARRAY
-    $post_arraylist = array();
-    $FinalArray = array();
-    $jsontype['JsonType'] = array();
+    $post_arraylist = array('JSONTYPE'=> 'RESPONSE');
+    // $post_arraylist['JsonType'] = array();
     $post_arraylist['DATOS_CLIENTE'] = array();
 
 
@@ -25,20 +24,31 @@ header('Content-Type: application/json');  //FOR USE JSON
     //TURN IT TO JSON & OUTPUT
     echo json_encode($post_arraylist);
 
-    //NO POST
-    echo json_encode(
-        array('message' => 'NO POST FOUND')
-    );
 
 
 
 
+    // //POST ARRAY
+    // $post_arraylist = array();
+    // $FinalArray = array();
+    // $jsontype['JsonType'] = array();
+    // $post_arraylist['DATOS_CLIENTE'] = array();
 
-echo json_encode(
-    array('message' => 'NO POST FOUND')
-);
 
-
+    //     $post_item = array(
+    //         'cedula_ruc' => '',
+    //         'nombres' => '',
+    //         'apellido_pat' =>'',
+    //         'apellido_mat' => '',
+    //         'fecha_registro' => ''
+    //     );
+    //     //PUSH TO DATA
+    //     array_push($post_arraylist['DATOS_CLIENTE'], $post_item);
+    //     array_push($FinalArray,$jsontype);
+    //     array_push($FinalArray,$post_arraylist['DATOS_CLIENTE']);
+        
+    // //TURN IT TO JSON & OUTPUT
+    // echo json_encode($FinalArray);
 
 
 
