e9146dc98250ce76efc0fba1583903fc32eb300d
deivitrix <67457502+deivitrix@users.noreply.github.com>
2020-09-24 16:54:44 -0500

    Update put_update_user.php

--- PHP_REST_API/api/put/put_update_user.php
+++ PHP_REST_API/api/put/put_update_user.php
@@ -1,11 +1,10 @@
 <?php
-//HEADERS
 header('Access-Control-Allow-Origin: *');  //COMPLETE PUBLIC API CHANCE TO PRIVATE<ASK FRANKLIN> 
 header('Content-Type: application/json');  //FOR USE JSON
 
 //INCLUDES
 include_once '../../config/Database.php';
-include_once '../../models/publications.php';
+include_once '../../models/user.php';
 
 //INSTANTIATE DB & CONNECT
 $database = new Database();
@@ -14,6 +13,7 @@ $db = $database->connect();
 
 //INSTANTIATE BLOG POST OBJECT
 $post = new User($db);
+
 
 if(isset($_GET['user_email']) && isset($_GET['password']) && isset($_GET['user_names']) && isset($_GET['user_last_names']) && isset($_GET['birthdate']) && isset($_GET['cellphone']) && isset($_GET['sex']) && isset($_GET['doctor_profession']))
 {
